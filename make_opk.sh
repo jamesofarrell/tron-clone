afcad7ecf139efdaf7b398fcc1fc41eca403acab
James OFarrell <jofarrell@gmail.com>
2019-10-20 16:45:03 +1000

    rg350 files

---
+++ make_opk.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+NAME=tron-clone
+OPK_NAME=${NAME}.opk
+
+echo ${OPK_NAME}
+
+# create default.gcw0.desktop
+cat > default.gcw0.desktop <<EOF
+[Desktop Entry]
+Name=tron-clone
+Comment=2 Player Light Cycle Game
+Exec=tron-clone.elf
+Terminal=false
+Type=Application
+StartupNotify=true
+Icon=tron-clone
+Categories=games;
+EOF
+
+FLIST="tron-clone.png tron-clone.elf Resources default.gcw0.desktop"
+
+# create opk
+
+rm -f ${OPK_NAME}
+mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
+
+cat default.gcw0.desktop
+rm -f default.gcw0.desktop
